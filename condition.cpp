e094279a35a1f487b1189d85e8ffe2c02acb289d
Vijay Singh <alenwalker@localhost.localdomain>
2025-03-25 08:36:23 +0530

    Condition

---
+++ condition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    int b = 12;
+    if (a > b)
+    {
+        cout << "a is greater than b" << endl;
+    }
+    else
+    {
+        cout << "b is greater than a" << endl;
+    }
+    return 0;
+}
