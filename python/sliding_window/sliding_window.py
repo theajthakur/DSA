7bc7b0422eae82b0322f008247706b2800479e1e
Vijay Singh <thakurvijayofficial@gmail.com>
2025-07-27 23:57:08 +0530

    modified

--- python/sliding_window/sliding_window.py
+++ python/sliding_window/sliding_window.py
@@ -1,15 +1,18 @@
-arr=[82,56,91,87,25,76,56]
+arr=[5]
 
-width=3
+
+
+width=1
 max=0
 current=0
 
-max=sum(arr[:width])
-    
+current=sum(arr[:width])
+max=current
 for x in range(0,len(arr)-width):
-    current=max-arr[x]+arr[width+x]
+    current+=arr[width+x]-arr[x]
     if current>max:
         max=current
 
-
+    print("max: ", max, "; current: ", current)
+    
 print(max)
