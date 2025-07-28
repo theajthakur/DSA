d334abb65d2100d6a78a0e8d97a06e3346fb6645
Vijay Singh <thakurvijayofficial@gmail.com>
2025-07-28 10:06:22 +0530

    Fixed Anti

--- python/array/circular_array.py
+++ python/array/circular_array.py
@@ -6,9 +6,9 @@ n = 2
 # Clockwise Rotation
 print("Clockwise Rotation:")
 for x in range(0, n*len(array)):
-    print(array[(x + n) % len(array)])
+    print(array[x % len(array)])
 
 # AntiClockwise Rotation
 print("\nAntiClockwise Rotation:")
 for x in range(0, n*len(array)):
-    print(array[(x - n) % len(array)])
+    print(array[len(array) - x%len(array) - 1])
