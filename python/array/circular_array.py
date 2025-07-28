8250fb932c98be1d5fd0495576083c23ee96a5ea
Vijay Singh <thakurvijayofficial@gmail.com>
2025-07-28 10:04:44 +0530

    Array rotation Arguments

--- python/array/circular_array.py
+++ python/array/circular_array.py
@@ -1,14 +1,14 @@
 array = [4, 8, 3, 9]
 
 # Number of rotations
-n = 10
+n = 2
 
 # Clockwise Rotation
 print("Clockwise Rotation:")
-for x in range(0, n):
+for x in range(0, n*len(array)):
     print(array[(x + n) % len(array)])
 
 # AntiClockwise Rotation
 print("\nAntiClockwise Rotation:")
-for x in range(0, n):
+for x in range(0, n*len(array)):
     print(array[(x - n) % len(array)])
